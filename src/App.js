ff2f0512af7693cb81fb1a326ba49ff616850182
karim <k.guezlane@gmail.com>
2021-08-06 16:52:17 +0100

    adding components using Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import ShopCart from "./components/Shop-cart";
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Nav />
+			</div>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/shop' component={Shop} />
+				<Route exact path='/shop-cart' component={ShopCart} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
