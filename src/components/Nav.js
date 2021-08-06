8bbeddfedde1dbae490f14a0023199359579108c
karim <k.guezlane@gmail.com>
2021-08-06 16:51:44 +0100

    setting up Nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Nav() {
+	return (
+		<ul id='Nav'>
+			<Link to='/'>
+				<div id='logo' className='flex-r pd-5px mrg-5px'></div>
+			</Link>
+			<div className='flex-r pd-5px mrg-5px'>
+				<Link to='/'>
+					<li>Home</li>
+				</Link>
+				<Link to='/shop'>
+					<li>Shop</li>
+				</Link>
+				<Link to='/shop-cart'>
+					<li>Cart</li>
+				</Link>
+			</div>
+		</ul>
+	);
+}
+export default Nav;
