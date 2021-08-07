3693e16c12023e952919238ef6b834e78dddc780
karim <k.guezlane@gmail.com>
2021-08-07 19:46:07 +0100

    created a sliding images component

---
+++ src/components/Home-components/Home-top-product.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { TopImages } from "../data/Top-images";
+function TopProducts() {
+	const [index, setIndex] = useState(0);
+	const [topImage, setTopImage] = useState(TopImages[index]);
+	const [isActive, setIsActive] = useState(index);
+
+	const changeImg = async (e) => {
+		if (e) {
+			setIndex(Number(e.target.id));
+			setIsActive(Number(e.target.id));
+			setTopImage(TopImages[Number(e.target.id)]);
+			return;
+		}
+		if (index < 3) {
+			setIndex((index) => index + 1);
+			setIsActive(index);
+			setTopImage(TopImages[index]);
+		} else {
+			setIndex(0);
+			setIsActive(index);
+			setTopImage(TopImages[index]);
+		}
+	};
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			changeImg();
+		}, 3000);
+		return () => clearInterval(interval);
+	});
+	return (
+		<div id='top-products' className='flex-c'>
+			<div className='image-slider'>
+				<img src={topImage} alt='Product' />
+			</div>
+			<div className='flex-r image-bullets'>
+				<i
+					onClick={changeImg}
+					id='0'
+					className={`far fa-circle ${isActive === 0 ? "on" : null}`}></i>
+				<i
+					onClick={changeImg}
+					id='1'
+					className={`far fa-circle ${isActive === 1 ? "on" : null}`}></i>
+				<i
+					onClick={changeImg}
+					id='2'
+					className={`far fa-circle ${isActive === 2 ? "on" : null}`}></i>
+				<i
+					onClick={changeImg}
+					id='3'
+					className={`far fa-circle ${isActive === 3 ? "on" : null}`}></i>
+			</div>
+		</div>
+	);
+}
+export { TopProducts };
