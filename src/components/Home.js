530cd89ea52f1e2b4d42e840c8d5d6617b924d16
karim <k.guezlane@gmail.com>
2021-08-07 19:37:40 +0100

    adding components to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
+import React from "react";
+import { HomeInfo } from "./Home-components/Home-info";
+import { TopProducts } from "./Home-components/Home-top-product";
+
 function Home() {
 	return (
-		<div id='home'>
-			<h1>Home</h1>
+		<div id='home' className='flex-r'>
+			<div id='homeContainer' className='flex-r'>
+				<HomeInfo />
+				<TopProducts />
+			</div>
 		</div>
 	);
 }
