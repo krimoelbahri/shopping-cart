d6dbb421d40832baec205b42da66734f38b98326
karim <k.guezlane@gmail.com>
2021-08-08 14:39:48 +0100

    side bar for the Shop component

---
+++ src/components/Shop-components/Side-bar.js
@@ -0,0 +1,16 @@
+function SideBar() {
+	return (
+		<div id='sideBar' className='flex-c'>
+			<h3 className='pd-10px'> CATEGORIES</h3>
+			<div id='catagories' className='flex-c '>
+				<h4 className='pd-5px'>All</h4>
+				<h4 className='pd-5px'>Hiking</h4>
+				<h4 className='pd-5px'>School</h4>
+				<h4 className='pd-5px'>men</h4>
+				<h4 className='pd-5px'>Women</h4>
+				<h4 className='pd-5px'>Kids</h4>
+			</div>
+		</div>
+	);
+}
+export { SideBar };
