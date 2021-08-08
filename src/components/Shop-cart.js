5fc549d67319b3b511e7e652520c86a7f66f1632
karim <k.guezlane@gmail.com>
2021-08-08 19:01:21 +0100

    managing my cart component

--- src/components/Shop-cart.js
+++ src/components/Shop-cart.js
@@ -1,7 +1,15 @@
-function ShopCart() {
+import { Address } from "./Cart-components/Address";
+import { CartItems } from "./Cart-components/Cart-items";
+import { CreditCard } from "./Cart-components/Credit-card";
+
+function ShopCart(props) {
 	return (
-		<div id='shopCart'>
-			<h1>ShopCart</h1>
+		<div id='shopCart' className='flex-r'>
+			<div id='shopCartContainer' className='flex-r'>
+				<CartItems cartItems={props.cartItems} />
+				<Address />
+				<CreditCard />
+			</div>
 		</div>
 	);
 }
