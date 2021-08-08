a3dca164be3ece441986ae1479a5ddb064f18b89
karim <k.guezlane@gmail.com>
2021-08-08 18:58:15 +0100

    added logic to add items to cart

--- src/components/Shop-components/Main-products.js
+++ src/components/Shop-components/Main-products.js
@@ -1,11 +1,40 @@
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { TopImages } from "../data/Top-images";
 
-function MainProducts() {
+function MainProducts(props) {
 	const [productsArray, setProductsArray] = useState([]);
+
+	const handleAddToCart = (e) => {
+		let val = Number(
+			e.target.parentNode.previousSibling.childNodes[1].value
+		);
+		if (val !== 0) {
+			let item = TopImages[e.target.id];
+			props.handleCartItems(val, item);
+			e.target.parentNode.previousSibling.childNodes[1].value = 0;
+		}
+	};
+
+	const handlePlus = (e) => {
+		e.target.previousSibling.value++;
+	};
+	const handleMinus = (e) => {
+		let val = e.target.nextSibling.value;
+		if (val !== "0") {
+			e.target.nextSibling.value--;
+		}
+	};
+	const handleBlur = (e) => {
+		if (!e.target.value) {
+			e.target.value = 0;
+		}
+	};
+
 	useEffect(() => {
 		setProductsArray(TopImages);
 	}, []);
+
 	return (
 		<div id='mainProducts' className='flex-r'>
 			{productsArray.map((product, i) => {
@@ -17,13 +46,20 @@ function MainProducts() {
 						<div className='product-name'>Product{i}</div>
 						<div className='product-price'>Price: {i}$</div>
 						<div className='quantity flex-r'>
-							<i className='fas fa-minus'></i>
-							<input type='number' value={0} />
-							<i className='fas fa-plus'></i>
+							<i onClick={handleMinus} className='fas fa-minus'></i>
+							<input onBlur={handleBlur} type='number' defaultValue={0} />
+							<i onClick={handlePlus} className='fas fa-plus'></i>
 						</div>
 						<div className='product-button-div flex-r'>
-							<button className='product-button'>Details</button>
-							<button className='product-button'>Add to cart</button>
+							<Link to={`/shop/${i}`}>
+								<button className='product-button'>Details</button>
+							</Link>
+							<button
+								id={i}
+								onClick={handleAddToCart}
+								className='product-button'>
+								Add to cart
+							</button>
 						</div>
 					</div>
 				);
