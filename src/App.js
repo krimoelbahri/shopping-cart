5c5e9396b872435d9fb458d8b8389e84f3ca5dd9
karim <k.guezlane@gmail.com>
2021-08-08 19:02:25 +0100

    handeling adding items to cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -8,14 +8,56 @@ import Items from "./components/Items";
 import "./Style/App.css";
 
 function App() {
+	const [cartItems, setCartItems] = useState([]);
+
+	const checkItemExist = (item) => {
+		for (let i = 0; i < cartItems.length; i++) {
+			if (cartItems[i].url === item) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	const handleCartItems = (quantity, item) => {
+		if (checkItemExist(item)) {
+			setCartItems((arr) => {
+				return arr.map((obj) => {
+					if (obj.url === item) {
+						obj = {
+							...obj,
+							quantity: obj.quantity + quantity,
+						};
+					}
+					return obj;
+				});
+			});
+			return;
+		}
+		let obj = {
+			url: item,
+			quantity,
+		};
+		setCartItems([...cartItems, obj]);
+	};
+
+	useEffect(() => {
+		console.log(cartItems);
+	}, [cartItems]);
 	return (
 		<BrowserRouter>
-			<Nav />
+			<Nav itemsNumber={cartItems.length} />
 			<Switch>
-				<Route exact path='/' component={Home} />
-				<Route exact path='/shop' component={Shop} />
-				<Route exact path='/shop-cart' component={ShopCart} />
-				<Route exact path='/:id' component={Items} />
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/shop'>
+					<Shop handleCartItems={handleCartItems} />
+				</Route>
+				<Route exact path='/shop-cart'>
+					<ShopCart cartItems={cartItems} />
+				</Route>
+				<Route exact path='/shop/:id' component={Items} />
 			</Switch>
 		</BrowserRouter>
 	);
