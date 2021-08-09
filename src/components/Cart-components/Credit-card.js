9e3d77885bc6ce7e5bdc4d420b7b55d515dd1630
karim <k.guezlane@gmail.com>
2021-08-09 14:23:06 +0100

    a credit card component

--- src/components/Cart-components/Credit-card.js
+++ src/components/Cart-components/Credit-card.js
@@ -1,4 +1,51 @@
 function CreditCard(props) {
-	return <div id='creditCard' className='flex-c'></div>;
+	return (
+		<div id='creditCard' className='flex-c'>
+			<h5 className='pd-10px mrg-10px'>Card Details</h5>
+			<div className='card-image-div'>
+				<i className='fas fa-credit-card'></i>
+			</div>
+			<div id='addressContainer' className='flex-c  mrg-10px'>
+				<div className='form-div flex-c'>
+					<label className='pd-5px' htmlFor='cardName'>
+						Name On Card
+					</label>
+					<input className='address-input' id='cardName' type='text' />
+				</div>
+				<div className='form-div flex-c '>
+					<label className='pd-5px' htmlFor='cardNumber'>
+						Card Number{" "}
+					</label>
+					<input className='address-input' id='cardNumber' type='number' />
+				</div>
+				<div className='form-div flex-r '>
+					<div className='sub-form-div flex-c '>
+						<label className='pd-5px' htmlFor='expiration'>
+							Expiration Date
+						</label>
+						<div className='flex-r '>
+							<input
+								className='address-input mrg-5px'
+								id='expiration-day'
+								type='text'
+							/>
+							<input
+								className='address-input mrg-5px'
+								id='expiration-month'
+								type='text'
+							/>
+						</div>
+					</div>
+					<div className='sub-form-div flex-c '>
+						<label className='pd-5px' htmlFor='cvv'>
+							CVV
+						</label>
+						<input className='address-input' id='cvv' type='text' />
+					</div>
+				</div>
+				<button id='checkOutButton'>Check Out</button>
+			</div>
+		</div>
+	);
 }
 export { CreditCard };
