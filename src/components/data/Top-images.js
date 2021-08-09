928c0597036c376d2266170ab0bc652e4a1341b3
karim <k.guezlane@gmail.com>
2021-08-09 14:22:18 +0100

    +

--- src/components/data/Top-images.js
+++
@@ -1,6 +0,0 @@
-export let TopImages = [
-	"https://dk0fkjygbn9vu.cloudfront.net/cache-buster-11626916655/deuter/mediaroom/product-images/backpacks/hiking-backpacks/image-thumb__39239__deuter_product-img/3410321-1323-SpeedLite22SL-w20-d0.png",
-	"https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2017/04/main/jansport-hatchet-backpack-sun-0417.jpg",
-	"https://i.pinimg.com/originals/f2/0e/f2/f20ef23c4acaf3196534dfbaa59995d3.jpg",
-	"https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_33/1600763/screen_shot_2020-08-14_at_8-43-03_am.png",
-];
