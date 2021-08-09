09fcc733e253444b76e933c7409f4593905eb6a7
karim <k.guezlane@gmail.com>
2021-08-09 14:25:49 +0100

    removed sideBar

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,8 @@
 import { MainProducts } from "./Shop-components/Main-products";
-import { SideBar } from "./Shop-components/Side-bar";
 
 function Shop(props) {
 	return (
 		<div id='shop' className='flex-r'>
-			<SideBar />
 			<MainProducts handleCartItems={props.handleCartItems} />
 		</div>
 	);
