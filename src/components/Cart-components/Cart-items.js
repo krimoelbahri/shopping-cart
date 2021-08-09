3d0b9ffeade4116bb3a707d43143620567059b09
karim <k.guezlane@gmail.com>
2021-08-09 14:23:32 +0100

    a component to show cart items

--- src/components/Cart-components/Cart-items.js
+++ src/components/Cart-components/Cart-items.js
@@ -2,19 +2,27 @@ function CartItems(props) {
 	return (
 		<div id='cartItems' className='flex-c'>
 			<h5 className='pd-10px mrg-10px'>SHOPPING CART</h5>
-			<div id='cartItemsContainer' className='flex-c  mrg-10px'>
+			<div id='cartItemsContainer' className='flex-c '>
 				{props.cartItems.map((product, i) => {
 					return (
-						<div key={i} className='cart-product-container flex-r mrg-10px'>
+						<div
+							key={product.id}
+							className='cart-product-container flex-r mrg-10px'>
 							<div className='cart-image-container'>
 								<img src={product.url} alt='Product' />
 							</div>
 							<div className='flex-c'>
-								<div className='product-name'>Product{i}</div>
-								<div className='product-price'>Price: {i}$</div>
+								<div className='product-name'>{product.title}</div>
+								<div className='product-price'>Price: {product.price}$</div>
 								<div className='product-quantity'>
 									Quantity: {product.quantity}
 								</div>
+							</div>
+							<div>
+								<i
+									className='far fa-trash-alt'
+									id={i}
+									onClick={props.deleteProduct}></i>
 							</div>
 						</div>
 					);
