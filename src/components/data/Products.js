37f34c9c1f624e55b4dc76202f9db9e8cebe49a6
karim <k.guezlane@gmail.com>
2021-08-09 14:22:47 +0100

    created prodcts data

---
+++ src/components/data/Products.js
@@ -0,0 +1,82 @@
+let TopProductsImages = [
+	"https://cdn.shopify.com/s/files/1/0260/1439/products/2048x2048_Dakota_Backpack_onyx_front_1024x1024_d5f8e71e-446a-408d-94ba-bd5b1cbbd070_1024x1024.jpg?v=1627058954",
+	"https://images.jansport.com/is/image/JanSport/JS0A4QVA_003_front?$WC-FULLIMAGE$",
+	"https://www.dhresource.com/0x0/f2/albu/g8/M01/83/65/rBVaVFv2cIuAATTjAAKUWd2G5PY522.jpg/fashionable-trendy-backpack-female-students.jpg",
+	"https://shop.highsierra.com/dw/image/v2/AAUE_PRD/on/demandware.static/-/Sites-product-catalog/default/dw4b3c7f84/collections/_highsierra/Backpacks/500x500/536466812_Loop_Front_Angle.jpg",
+];
+let AllProducts = [
+	{
+		url: "https://cdn.shopify.com/s/files/1/0260/1439/products/2048x2048_Dakota_Backpack_onyx_front_1024x1024_d5f8e71e-446a-408d-94ba-bd5b1cbbd070_1024x1024.jpg?v=1627058954",
+		title: "Neoprene Backpack",
+		price: 19.99,
+		id: "Neoprene0",
+	},
+	{
+		url: "https://www.dhresource.com/0x0/f2/albu/g8/M01/83/65/rBVaVFv2cIuAATTjAAKUWd2G5PY522.jpg/fashionable-trendy-backpack-female-students.jpg",
+		title: "Fashionable Backpack",
+		price: 19.99,
+		id: "Backpack0",
+	},
+	{
+		url: "https://shop.highsierra.com/dw/image/v2/AAUE_PRD/on/demandware.static/-/Sites-product-catalog/default/dw4b3c7f84/collections/_highsierra/Backpacks/500x500/536466812_Loop_Front_Angle.jpg",
+		title: "Loop Backpack",
+		price: 19.99,
+		id: "Backpack1",
+	},
+	{
+		url: "https://images.jansport.com/is/image/JanSport/JS0A4QVA_003_front?$WC-FULLIMAGE$",
+		title: "JanSport",
+		price: 19.99,
+		id: "JanSport0",
+	},
+	{
+		url: "https://www.luluhypermarket.com/medias/1530075-01.jpg-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wzMDEzMTh8aW1hZ2UvanBlZ3xoMGMvaGMyLzEwNjE1Njg3NTQ0ODYyLzE1MzAwNzUtMDEuanBnXzEyMDBXeDEyMDBIfGIwZTgyMGNiMDM3N2VmZGNkNWI1YTdjMzJkYzljMzRmMDhlZmViZGJkOTg5OWVjNzYyYzUxZDA4NmViZmUxN2I",
+		title: "Laptop Backpack",
+		price: 19.99,
+		id: "Backpack2",
+	},
+	{
+		url: "https://images.squarespace-cdn.com/content/v1/55468f6ce4b099a7d92ab340/1585081665238-FAA1JYUNW0BKG5RBOAM4/IVAR+Urban+20+Comfortable+Backpack",
+		title: "comfortable backpack",
+		price: 19.99,
+		id: "backpack3",
+	},
+	{
+		url: "https://i.insider.com/6080877fc36a8a00185c6419?width=1000&format=jpeg&auto=webp",
+		title: "Dagne Dover",
+		price: 29.99,
+		id: "Neoprene1",
+	},
+	{
+		url: "https://mldvwwasb8tu.i.optimole.com/Q9dV3Lk.tb2a~61eeb/w:1400/h:933/q:90/https://mk0travelawayrru2xew.kinstacdn.com/wp-content/uploads/2020/11/bellroy-transit-backpack-plus.jpg",
+		title: "Bellroy Backpack",
+		price: 29.99,
+		id: "Bellroy0",
+	},
+	{
+		url: "https://cdn2.expertreviews.co.uk/sites/expertreviews/files/styles/er_main_wide/public/2019/10/best-travel-backpack_osprey-red.png?itok=GRNsHIng",
+		title: "Osprey Transporter",
+		price: 29.99,
+		id: "Osprey0",
+	},
+	{
+		url: "https://ae01.alicdn.com/kf/Hfef4d28e39fd4c3caefe3ee912d8316dI/80L-Camping-Hiking-Backpacks-Big-Outdoor-Bag-Backpack-Nylon-superlight-Sport-Travel-Bag-Aluminum-alloy-support.jpg_Q90.jpg_.webp",
+		title: "Hiking Backpack",
+		price: 39.99,
+		id: "Backpack4",
+	},
+	{
+		url: "https://p.globalsources.com/IMAGES/PDT/BIG/517/B1171950517.jpg",
+		title: "Hiking Backpack",
+		price: 39.99,
+		id: "Backpack5",
+	},
+	{
+		url: "https://i5.walmartimages.com/asr/b127d50e-3167-437a-9209-032950438a62_1.7630fe143f503b42a4620bacd79d233f.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
+		title: "Hiking Backpack",
+		price: 39.99,
+		id: "Backpack6",
+	},
+];
+
+export { TopProductsImages, AllProducts };
